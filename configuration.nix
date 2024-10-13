fa4da4a824f76e1ee8e491c19c9987592dbf6a5c
Luv Mehta <mehluv@gmail.com>
2024-10-13 23:27:59 +0530

    Added GC

--- configuration.nix
+++ configuration.nix
@@ -46,6 +46,12 @@
     extraGroups = [ "networkmanager" "wheel" ];
   };
 
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 1w";
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
