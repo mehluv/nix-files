3cca28dc3dda9d0cb57a8d194a4019d2f6e09930
Luv Mehta <mehluv@gmail.com>
2024-10-14 22:06:21 +0530

    Login Screen

--- kde.nix
+++ kde.nix
@@ -1,7 +1,22 @@
-{ ... }:
+{ pkgs, ... }:
 {
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
+  environment.systemPackages = with pkgs; [
+    (catppuccin-sddm.override {
+      flavor = "mocha";
+      font  = "Inclusive Sans";
+      fontSize = "10";
+      background = "${./wallpaper.jpg}";
+      loginBackground = true;
+    })
+  ];
+
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+    autoNumlock = true;
+    theme = "catppuccin-mocha";
+  };
+
   services.desktopManager.plasma6.enable = true;
 }
